7948e75851304e5c320e8d3475532fd066408bdb
diwadeep <141811766+diwadeep@users.noreply.github.com>
2023-08-15 01:08:11 +0530

    12 files modified, 2 deleted

--- src/app/modules/quiz-app-module/quiz-app-module.module.ts
+++ src/app/modules/quiz-app-module/quiz-app-module.module.ts
@@ -6,17 +6,8 @@ import { ResultComponent } from './components/result/result.component';
 import { QuizEntryComponent } from './components/quiz-entry/quiz-entry.component';
 import { QuizAppModuleRoutingModule } from './quiz-app-module.routing.module';
 
-
 @NgModule({
-  declarations: [
-    QuizEntryComponent,
-    QuestionComponent,
-    ResultComponent
-  ],
-  imports: [
-    CommonModule,
-    QuizAppModuleRoutingModule,
-    ReactiveFormsModule
-  ]
+  declarations: [QuizEntryComponent, QuestionComponent, ResultComponent],
+  imports: [CommonModule, QuizAppModuleRoutingModule, ReactiveFormsModule],
 })
-export class QuizModuleModule { }
+export class QuizAppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,11 @@
-// import './polyfills';
-
-// import { enableProdMode } from '@angular/core';
-// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-// import { AppModule } from './app/app.module';
-
-// platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-//   // Ensure Angular destroys itself on hot reloads.
-//   if (window['ngRef']) {
-//     window['ngRef'].destroy();
-//   }
-//   window['ngRef'] = ref;
-
-//   // Otherwise, log the boot error
-// }).catch(err => console.error(err));
-
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
-//import { environment } from './environments/environment';
+import { environment } from './environment/environment';
 
-// if (environment.production) {
-//   enableProdMode();
-// }
+if (environment.production) {
+  enableProdMode();
+}
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
