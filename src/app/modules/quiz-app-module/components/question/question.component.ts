06a0c362a1cf33cd7a625108651877df3e7290b1
diwadeep <141811766+diwadeep@users.noreply.github.com>
2023-08-14 17:57:20 +0530

    Initial commit

---
+++ src/app/modules/quiz-app-module/components/question/question.component.ts
@@ -0,0 +1,83 @@
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  ElementRef,
+  ViewChild,
+  AfterViewInit,
+} from '@angular/core';
+import { Question } from '../../models/question';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-question',
+  templateUrl: './question.component.html',
+  styleUrls: ['./question.component.scss'],
+})
+export class QuestionComponent implements OnInit, OnChanges, AfterViewInit {
+  @Input() displayAnswer: Boolean;
+  @Input() questionList: Question[];
+  @Input() totalCorrectAnswer: Number[];
+  totalAnsweredQuestion: number = 0;
+  @ViewChild('finalResultText') finalResultText: ElementRef;
+
+  constructor(private router: Router) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.totalAnsweredQuestion = 0;
+  }
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    if (this.displayAnswer && this.questionList.length > 0) {
+      const nativeElement = this.finalResultText.nativeElement;
+      if (this.totalCorrectAnswer.length <= 1) {
+        nativeElement.classList.add('red-label');
+      } else if (
+        this.totalCorrectAnswer.length > 1 &&
+        this.totalCorrectAnswer.length <= 3
+      ) {
+        nativeElement.classList.add('yellow-label');
+      } else if (this.totalCorrectAnswer.length > 3) {
+        nativeElement.classList.add('green-label');
+      }
+    }
+  }
+
+  selectAnswer(i: number, answer: string) {
+    if (this.displayAnswer == false) {
+      if (!this.questionList[i].user_selected_answer) {
+        this.totalAnsweredQuestion++;
+      }
+      this.questionList[i].user_selected_answer = answer;
+      if (
+        this.questionList[i].correct_answer == this.questionList[i].user_selected_answer
+      ) {
+        this.totalCorrectAnswer.push(i);
+      } else {
+        const indexKey = this.totalCorrectAnswer.indexOf(i);
+        if (indexKey > -1) {
+          this.totalCorrectAnswer.splice(indexKey, 1);
+        }
+      }
+    }
+  }
+
+  createNewQuiz() {
+    localStorage.removeItem('question');
+    localStorage.removeItem('totalCorrectAnswer');
+    this.router.navigate(['']);
+  }
+
+  submitAnwser() {
+    localStorage.setItem('question', JSON.stringify(this.questionList));
+    localStorage.setItem(
+      'totalCorrectAnswer',
+      JSON.stringify(this.totalCorrectAnswer)
+    );
+    this.router.navigateByUrl('/result');
+  }
+}
---
+++ src/app/modules/quiz-app-module/components/quiz-entry/quiz-entry.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit } from '@angular/core';
+import { Category } from '../../models/category';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Question, QuestionParams } from '../../models/question';
+import { QuizAppService } from '../../services/quiz.app.service';
+
+@Component({
+  selector: 'app-quiz-entry',
+  templateUrl: './quiz-entry.component.html',
+  styleUrls: ['./quiz-entry.component.scss'],
+})
+
+export class QuizEntryComponent implements OnInit {
+  
+  difficultyLevelList: String[];
+  questionCategoryList: Category[] = [];
+  questionList: Question[] = [];
+  quizForm: FormGroup;
+  displayQuestion: Boolean;
+  
+  constructor(
+    private quizService: QuizAppService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.buildQuizForm();
+    this.difficultyLevelList = this.quizService.questionLevels;
+    this.getQuestionCategoryList();
+  }
+
+  buildQuizForm() {
+    this.quizForm = this.formBuilder.group({
+      categorySelect: ['', [Validators.required]],
+      difficultyLevelSelect: ['', [Validators.required]],
+    });
+  }
+
+  getQuestionCategoryList() {
+    this.quizService.getQuestionCategoryList().subscribe(
+      (response) => {
+        this.questionCategoryList = response;
+      },
+      (error) => {
+        console.log(error);
+        alert("There is some problem occurred while fetching the question category list. Please try after some time.");
+      }
+    );
+  }
+
+  getQuestionList(params: QuestionParams) {
+    this.quizService.getQuestionList(params).subscribe(
+      (response) => {
+        if (response.length > 0) {
+          this.displayQuestion = true;
+          this.questionList = response;
+        }
+      },
+      (error) => {
+        console.log(error);
+        alert("There is some problem occurred while fetching the question list. Please try after some time.");
+      }
+    );
+  }
+
+  onSubmitQuizForm() {
+    if (this.quizForm.valid) {
+      const params = {
+        category: this.quizForm.controls['categorySelect'].value,
+        difficulty: this.quizForm.controls['difficultyLevelSelect'].value,
+      };
+      this.getQuestionList(params);
+    }
+  }
+}
